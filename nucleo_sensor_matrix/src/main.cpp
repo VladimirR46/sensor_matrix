90b2a9ccd52f8b8f3820fdd553fad0e602c65ce3
VladimirR46 <vovik_0_1@mail.ru>
2019-09-02 22:02:20 +0300

    All work

--- nucleo_sensor_matrix/src/main.cpp
+++ nucleo_sensor_matrix/src/main.cpp
@@ -1,7 +1,29 @@
 #include <mbed.h>
+#include <EthernetInterface.h> // Network interface API
+#include "UDPSocket.h"         // Network socket API
 
 #define DIGITAL_OUT 9
 #define ANALOG_IN 6
+
+#define _STATIC_IP // enable static IP (dhcp is used instead)
+#define _SERVER_PC
+
+#ifdef _STATIC_IP
+#define _IP "192.168.0.101"    //"169.254.4.96" //"12.0.3.247"
+#define _GATEWAY "192.168.0.1" //"169.254.255.255"//"12.0.0.1"
+#define _MASK "255.255.255.0"  //"255.255.0.0"  //"255.255.248.0"
+#endif
+
+#ifdef _SERVER_PC
+#define _HOST_IP "192.168.0.4" //"169.254.5.149"//"192.168.0.100"
+#define _HOST_PORT 4442
+#endif
+
+// API objects
+
+EthernetInterface eth;
+UDPSocket udp_socket(&eth);
+SocketAddress sockAddr(_HOST_IP, _HOST_PORT);
 
 Serial usb(USBTX, USBRX, 115200);
 
@@ -14,8 +36,18 @@ unsigned short Matrix[DIGITAL_OUT][ANALOG_IN];
 
 Timer send_timer;
 
+// functions prototypes
+void startEth(); // set network,
+
+unsigned int hz = 0;
+
+const uint8_t mess_size = ANALOG_IN * 2 + 1;
+unsigned char buff[mess_size];
+
 int main()
 {
+  startEth();
+
   // Init
   for (int i = 0; i < DIGITAL_OUT; i++)
     digital_pin[i] = 0;
@@ -28,20 +60,62 @@ int main()
     {
       digital_pin[i] = 1;
       wait_us(100);
+
+      buff[0] = i;
       for (int j = 0; j < ANALOG_IN; j++)
       {
         // Reads
-        Matrix[i][j] = analog_pin[j].read_u16();
+        uint16_t data = analog_pin[j].read_u16();
+
+        //Pack
+        buff[j + 1] = data & 0xFF00;
+        buff[j + 2] = data & 0x00FF;
+
+        //Matrix[i][j] = buff[j];
       }
       digital_pin[i] = 0;
+
+      // Send
+      udp_socket.send(buff, mess_size);
     }
 
+    hz++;
     int col = 7;
-    if (send_timer.read_ms() >= 100)
+    if (send_timer.read_ms() >= 1000)
     {
       send_timer.reset();
-      usb.printf("A0: %d  %d  %d  %d  %d  %d  \r\n", Matrix[col][0], Matrix[col][1], Matrix[col][2], Matrix[col][3], Matrix[col][4], Matrix[col][5]);
+      usb.printf("hz: %d \n", hz);
+      hz = 0;
+      //udp_socket.send("y", 1);
+      //usb.printf("A0: %d  %d  %d  %d  %d  %d  \r\n", Matrix[col][0], Matrix[col][1], Matrix[col][2], Matrix[col][3], Matrix[col][4], Matrix[col][5]);
     }
     //wait(0.1);
   }
+}
+
+//-----------------------------------------------------------------------
+void startEth()
+{
+
+#ifdef _STATIC_IP
+  printf("Setting network...\n");
+  if (eth.set_network(_IP, _MASK, _GATEWAY))
+    printf("Network can't be set\n");
+  else
+    printf("Network is set\n");
+#endif
+
+  printf("Connecting...\n");
+  if (eth.connect())
+    printf("Connection failure\n");
+  else
+    printf("Connection succeed\n");
+
+  const char *ip = eth.get_ip_address();
+  const char *mac = eth.get_mac_address();
+  printf("IP address is: %s\n", ip ? ip : "No IP");
+  printf("MAC address is: %s\n", mac ? mac : "No MAC");
+
+  udp_socket.connect(sockAddr);
+  udp_socket.set_blocking(false); // set_timeout(0) is equivalent to set_blocking(false) set_timeout(-1) is equivalent to set_blocking(true)
 }
